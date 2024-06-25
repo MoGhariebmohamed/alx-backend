1ba48196ce591a0a5fb0e149d78fffe49f363c01
Mo Gharieb <m.gharieb89@gmail.com>
2024-06-25 05:22:58 +0300

    t1v4

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -3,6 +3,9 @@
 this is pagination function
 """
 from typing import Tuple
+import csv
+import math
+from typing import List
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
@@ -13,10 +16,34 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     end = start + page_size
     return (start, end)
 
-res = index_range(1, 7)
-print(type(res))
-print(res)
 
-res = index_range(page=3, page_size=15)
-print(type(res))
-print(res)
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        to get pages of date
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        date = self.dataset()
+        if start > len(date):
+            return[]
+        return date[start:end]
