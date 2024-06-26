cd3ba6e6d5e48498d89b55c17eb6accef785c7b3
Mo Gharieb <m.gharieb89@gmail.com>
2024-06-26 20:08:40 +0300

    t3v3

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -36,4 +36,6 @@ class LRUCache(BaseCaching):
         """
         if not key:
             return None
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
         return self.cache_data.get(key, None)
