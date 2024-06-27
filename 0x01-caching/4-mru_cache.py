a5550dfc97aaf530ec5b2f6a2cd122396e5286e5
Mo Gharieb <m.gharieb89@gmail.com>
2024-06-27 03:47:27 +0300

    t4v1

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -6,7 +6,7 @@ from base_caching import BaseCaching
 from collections import OrderedDict
 
 
-class LRUCache(BaseCaching):
+class MRUCache(BaseCaching):
     """
     cashing system inherted from base class
     """
@@ -24,11 +24,14 @@ class LRUCache(BaseCaching):
         """
         if not key or not item:
             return
-        if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-            least_recent_use, any = self.cache_data.popitem(True)
-            print("DISCARD:", least_recent_use)
-        self.cache_data[key] = item
-        self.cache_data.move_to_end(key, last=False)
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                most_recent_use, any = self.cache_data.popitem(False)
+                print("DISCARD:", most_recent_use)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
         """
@@ -36,30 +39,6 @@ class LRUCache(BaseCaching):
         """
         if not key:
             return None
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
         return self.cache_data.get(key, None)
-my_cache = LRUCache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.put("D", "School")
-my_cache.print_cache()
-print(my_cache.get("B"))
-my_cache.put("E", "Battery")
-my_cache.print_cache()
-my_cache.put("C", "Street")
-my_cache.print_cache()
-print(my_cache.get("A"))
-print(my_cache.get("B"))
-print(my_cache.get("C"))
-my_cache.put("F", "Mission")
-my_cache.print_cache()
-my_cache.put("G", "San Francisco")
-my_cache.print_cache()
-my_cache.put("H", "H")
-my_cache.print_cache()
-my_cache.put("I", "I")
-my_cache.print_cache()
-my_cache.put("J", "J")
-my_cache.print_cache()
-my_cache.put("K", "K")
-my_cache.print_cache()
