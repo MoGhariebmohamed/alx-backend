bed8536df1189140b44c0ec517d57be8c08528f8
Mo Gharieb <m.gharieb89@gmail.com>
2024-07-05 23:32:09 +0300

    app4 rev1

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+this is flask configuration
+"""
+from flask_babel import Babel
+from flask import Flask, render_template, request
+
+
+class Config(object):
+    """
+    to configrate the babel app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    to get and detrmine the locator
+    """
+    locator = request.args.get('locale')
+    if locator in app.config['LANGUAGES']:
+        return locator
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    index to handle the route
+    """
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
