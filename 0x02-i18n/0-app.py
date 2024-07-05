6c6e6d46bb01ebe4db77f97651680a7254a1a37d
Mo Gharieb <m.gharieb89@gmail.com>
2024-07-05 09:49:54 +0300

    task app0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+this is flask app
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    index to handle the route
+    """
+    return render_template(''0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
+
