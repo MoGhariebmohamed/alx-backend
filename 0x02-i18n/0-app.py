d1d95b8108a13fe5d5c142bf314827e4cb7f4c97
Mo Gharieb <m.gharieb89@gmail.com>
2024-07-05 10:03:49 +0300

    task app2

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -13,7 +13,7 @@ def index() -> str:
     """
     index to handle the route
     """
-    return render_template(''0-index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
