487f88157184271d9103ac81785d7c712a2db9a8
Mo Gharieb <m.gharieb89@gmail.com>
2024-07-05 23:52:51 +0300

    app5 rev1

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""
+this is flask configuration
+"""
+from flask_babel import Babel
+from flask import Flask, render_template, request
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config(object):
+    """
+    to configrate the babel app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+def get_user():
+    """
+    Returns a user dictionary or None if ID value 
+    """
+    user_id = request.args('login_as', None)
+    if user_idid is not None and int(user_id) in users.keys():
+        return users.get(int(user_id))
+    return None
+
+
+@app.before_request
+def before_request():
+    """
+    Add user to flask.g if user is found
+    """
+    user = get_user()
+    g.user = user
+
+
+@babel.localeselector
+def get_locale():
+    """
+    to get and detrmine the locator
+    """
+    locator = request.args.get('locale')
+    if locator in app.config['LANGUAGES']:
+        return locator
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    index to handle the route
+    """
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
